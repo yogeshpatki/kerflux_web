8f6af45cfd01f9cf730d69a92070dac377781e9c
Yogesh Patki <yogesh.patki@gmail.com>
2019-11-12 00:59:01 +0530

    minor tweaks

--- src/lib/waves.js
+++ src/lib/waves.js
@@ -5,7 +5,7 @@ const R = Math.random;
 const F = Math.floor;
 export const getSinWave = () => {
     let wave = [];
-    let period = (F(R() * 10000) % 50) + 1;
+    let period = (F(R() * 10000) % 25) + 1;
     console.log(`period: ${period}`);
     for(let i=0; i<1000;i++) {
         wave.push(50 + (50 * S(PI * 2 * period*i/1000)));
